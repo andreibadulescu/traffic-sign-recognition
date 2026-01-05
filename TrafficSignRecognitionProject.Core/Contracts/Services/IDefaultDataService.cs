43fa8dfb6e878e8df1298172d033709c114e151b
Andrei Bădulescu <87699729+andreibadulescu@users.noreply.github.com>
2026-01-05 21:43:11 +0100

    Delete TrafficSignRecognitionProject.Core directory
    Files were moved to TrafficSignRecognitionProject directory for easier management.

--- TrafficSignRecognitionProject.Core/Contracts/Services/IDefaultDataService.cs
+++
@@ -1,8 +0,0 @@
-﻿using TrafficSignRecognitionProject.Core.Models;
-
-namespace TrafficSignRecognitionProject.Core.Contracts.Services;
-
-public interface IDefaultDataService
-{
-    Task<IEnumerable<Sign>> GetGridDataAsync();
-}
