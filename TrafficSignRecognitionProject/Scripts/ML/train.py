0a82608392845f28dc6262f18a602c2fec473cdf
karinaantoniu <karina.antoniu@gmail.com>
2026-01-17 20:05:00 +0200

    update all files for cuda

--- TrafficSignRecognitionProject/Scripts/ML/train.py
+++ TrafficSignRecognitionProject/Scripts/ML/train.py
@@ -283,7 +283,7 @@ def main():
         shuffle=True,
         pin_memory=False,
         drop_last=False,
-        num_workers=0
+        num_workers=4
     )
 
 	for epoch in range(config.EPOCHS):
