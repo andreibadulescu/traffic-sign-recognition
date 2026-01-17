6475744fb9b95be2cc1edceb1270dd01c1bea7b1
Karina Antoniu <karina.antoniu@gmail.com>
2026-01-17 17:28:04 +0200

    update model

--- TrafficSignRecognitionProject/Scripts/ML/model.py
+++ TrafficSignRecognitionProject/Scripts/ML/model.py
@@ -1,151 +1,139 @@
-# 1st module
+# 1st module 
 
-# import torch
-# import torch.nn as nn
-
-# When creating a new neural network, you would usually go about creating a new class and inheriting from nn.Module, and defining two methods: __init__ (the initializer, where you define your layers) and forward (the inference code of your module, where you use your layers). That's all you need, since PyTorch will handle backward pass with Autograd.
-# ACTUAL YOLO V1 MODEL
-
-# class YoloV1(nn.Module):
-#     def __init__(self):
-#         super().__init__()
-#         self.S = 7 # size i will resize the image
-#         self.C = 45 # number of classes i want to detect (nr of != trafic signs)
-#         self.B = 2 # number of bounding boxes each cell from the grill will try to predict (one for high boxes and one for wide boxes)
-#         self.outputDim = self.B * 5 + self.C # the length of the prediction vector for one grid cell
-
-#         # for each box the model has to predict 5 things: x (center coord), y, w (width), h (height), confidence (the confidence that this box contains an obj)
-#         # add to it the type of object it is
-
-#         #the filters needed for the model
-#         # out_channels are the number of filters
-#         self.block1 = nn.Sequential(
-#             nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(7, 7), stride=(2, 2), padding=3),
-#             nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
-#         )
-
-#         self.block2 = nn.Sequential(
-#             nn.Conv2d(in_channels=64, out_channels=192, kernel_size=(3, 3), padding='same'),
-#             nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
-#         )
-
-#         self.block3 = nn.Sequential(
-#             nn.Conv2d(in_channels=192, out_channels=128, kernel_size=(1, 1), padding='same'),
-#             nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3, 3), padding='same'),
-#             nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(1, 1), padding='same'),
-#             nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(3, 3), padding='same'),
-#             nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
-#         )
-
-#         self.block4 = nn.Sequential(
-#             nn.Conv2d(in_channels=512, out_channels=256, kernel_size=(1, 1), padding='same'),
-#             nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(3, 3), padding='same'),
-#             nn.Conv2d(in_channels=512, out_channels=256, kernel_size=(1, 1), padding='same'),
-#             nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(3, 3), padding='same'),
-#             nn.Conv2d(in_channels=512, out_channels=256, kernel_size=(1, 1), padding='same'),
-#             nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(3, 3), padding='same'),
-#             nn.Conv2d(in_channels=512, out_channels=256, kernel_size=(1, 1), padding='same'),
-#             nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(3, 3), padding='same'),
-#             nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(1, 1), padding='same'),
-#             nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=(3, 3), padding='same'),
-#             nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
-#         )
-
-#         self.block5 = nn.Sequential(
-#             nn.Conv2d(in_channels=1024, out_channels=512, kernel_size=(1, 1), padding='same'),
-#             nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=(3, 3), padding='same'),
-#             nn.Conv2d(in_channels=1024, out_channels=512, kernel_size=(1, 1), padding='same'),
-#             nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=(3, 3), padding='same'),
-#             nn.Conv2d(in_channels=1024, out_channels=1024, kernel_size=(3, 3), padding='same'),
-#             nn.Conv2d(in_channels=1024, out_channels=1024, kernel_size=(3, 3), padding=1, stride=(2, 2)),
-#         )
-
-#         self.block6 = nn.Sequential(
-#             nn.Conv2d(in_channels=1024, out_channels=1024, kernel_size=(3, 3), padding='same'),
-#             nn.Conv2d(in_channels=1024, out_channels=1024, kernel_size=(3, 3), padding='same')
-#         )
-
-#         self.block7 = nn.Sequential(
-#             nn.Flatten(),
-#             nn.Linear(1024 * self.S * self.S, 4096),
-#             nn.LeakyReLU(0.1),
-#             nn.Dropout(0.5),
-#             nn.Linear(4096, self.S * self.S * self.outputDim),
-#             nn.Sigmoid()
-#         )
-
-#     def forward(self, x):
-#             x = self.block1(x)
-#             x = self.block2(x)
-#             x = self.block3(x)
-#             x = self.block4(x)
-#             x = self.block5(x)
-#             x = self.block6(x)
-#             x = self.block7(x)
-#             x = x.reshape(-1, self.S, self.S, self.outputDim)
-
-#             return x
+# When creating a new neural network, you would usually go about creating a new class and inheriting from nn.Module, and defining two methods:
+# __init__ (the initializer, where you define your layers) and forward (the inference code of your module, where you use your layers).
+# That's all you need, since PyTorch will handle backward pass with Autograd. 
 
 
 import torch
 import torch.nn as nn
 
+
+class CNNBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, **kwargs):
+        super(CNNBlock, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
+        self.batchnorm = nn.BatchNorm2d(out_channels)
+        self.leakyrelu = nn.LeakyReLU(0.1)
+
+    def forward(self, x):
+        return self.leakyrelu(self.batchnorm(self.conv(x)))
+
 class YoloV1(nn.Module):
-    def __init__(self, nrClasses=15):
-        super().__init__()
-        self.S = 7
-        self.C = nrClasses
-        self.B = 2
+    def __init__(self, split_size=7, num_boxes=2, num_classes=15):
+        super(YoloV1, self).__init__()
+        self.S = split_size
+        self.B = num_boxes
+        self.C = num_classes
         self.outputDim = self.B * 5 + self.C
 
-        self.block1 = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=(7, 7), stride=(2, 2), padding=3),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            nn.LeakyReLU(0.1)
+        self.darknet = nn.Sequential(
+            # Block 1
+            CNNBlock(3, 64, kernel_size=7, stride=2, padding=3),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # Block 2
+            CNNBlock(64, 192, kernel_size=3, padding=1),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # Block 3
+            CNNBlock(192, 128, kernel_size=1),
+            CNNBlock(128, 256, kernel_size=3, padding=1),
+            CNNBlock(256, 256, kernel_size=1),
+            CNNBlock(256, 512, kernel_size=3, padding=1),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # Block 4
+            CNNBlock(512, 256, kernel_size=1),
+            CNNBlock(256, 512, kernel_size=3, padding=1),
+            CNNBlock(512, 256, kernel_size=1),
+            CNNBlock(256, 512, kernel_size=3, padding=1),
+            CNNBlock(512, 256, kernel_size=1),
+            CNNBlock(256, 512, kernel_size=3, padding=1),
+            CNNBlock(512, 256, kernel_size=1),
+            CNNBlock(256, 512, kernel_size=3, padding=1),
+            CNNBlock(512, 512, kernel_size=1),
+            CNNBlock(512, 1024, kernel_size=3, padding=1),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # Block 5
+            CNNBlock(1024, 512, kernel_size=1),
+            CNNBlock(512, 1024, kernel_size=3, padding=1),
+            CNNBlock(1024, 512, kernel_size=1),
+            CNNBlock(512, 1024, kernel_size=3, padding=1),
+            CNNBlock(1024, 1024, kernel_size=3, padding=1),
+            CNNBlock(1024, 1024, kernel_size=3, stride=2, padding=1),
+
+            CNNBlock(1024, 1024, kernel_size=3, padding=1),
+            CNNBlock(1024, 1024, kernel_size=3, padding=1),
         )
 
-        self.block2 = nn.Sequential(
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 3), padding=1),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            nn.LeakyReLU(0.1)
-        )
-
-        self.block3 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding=1),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), padding=1),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            nn.LeakyReLU(0.1)
-        )
-
-        self.block4 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), padding=1),
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), padding=1),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            nn.LeakyReLU(0.1)
-        )
-
-        self.block5 = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3, 3), padding=1),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3, 3), padding=1, stride=(2, 2)),
-            nn.LeakyReLU(0.1)
-        )
-
-        self.block_final = nn.Sequential(
+        # fully conected 
+        self.head = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(256 * 7 * 7, 256),
+            nn.Linear(1024 * self.S * self.S, 4096),
             nn.LeakyReLU(0.1),
             nn.Dropout(0.5),
-            nn.Linear(256, self.S * self.S * self.outputDim),
-            nn.Sigmoid()
+            nn.Linear(4096, self.S * self.S * self.outputDim) 
         )
 
     def forward(self, x):
-        x = self.block1(x)
-        x = self.block2(x)
-        x = self.block3(x)
-        x = self.block4(x)
-        x = self.block5(x)
-        x = self.block_final(x)
+        x = self.darknet(x)
+        x = self.fcs(x)
+        return x.reshape(-1, self.S, self.S, self.outputDim)
 
-        x = x.reshape(-1, self.S, self.S, self.outputDim)
-        return x
+# import torch
+# import torch.nn as nn
+# import config
+
+# class YoloV1(nn.Module):
+#     def __init__(self):
+#         super().__init__()
+#         self.S = config.S  # 7
+#         self.C = config.C
+#         self.B = config.B
+#         self.outputDim = self.B * 5 + self.C
+
+#         def conv_block(in_c, out_c, kernel, stride, padding):
+#             return nn.Sequential(
+#                 nn.Conv2d(in_c, out_c, kernel, stride, padding, bias=False),
+#                 nn.BatchNorm2d(out_c),
+#                 nn.LeakyReLU(0.1)
+#             )
+
+#         self.features = nn.Sequential(
+#             conv_block(3, 32, 3, 1, 1), 
+#             nn.MaxPool2d(2, 2), 
+
+#             conv_block(32, 64, 3, 1, 1),
+#             nn.MaxPool2d(2, 2), 
+
+#             conv_block(64, 128, 3, 1, 1),
+#             conv_block(128, 128, 3, 1, 1),
+#             nn.MaxPool2d(2, 2), 
+
+#             conv_block(128, 256, 3, 1, 1),
+#             conv_block(256, 256, 3, 1, 1),
+#             nn.MaxPool2d(2, 2), 
+
+#             conv_block(256, 512, 3, 1, 1),
+#             conv_block(512, 512, 3, 1, 1),
+#             nn.MaxPool2d(2, 2), 
+            
+#             conv_block(512, 1024, 3, stride=2, padding=1), 
+#             conv_block(1024, 1024, 3, stride=1, padding=1),
+#         )
+
+#         self.fcs = nn.Sequential(
+#             nn.Flatten(),
+#             nn.Linear(1024 * 7 * 7, 4096), 
+#             nn.LeakyReLU(0.1),
+#             nn.Dropout(0.5),
+#             nn.Linear(4096, self.S * self.S * self.outputDim),
+#         )
+
+#     def forward(self, x):
+#         x = self.features(x)
+#         x = self.fcs(x)
+#         x = x.reshape(-1, self.S, self.S, self.outputDim)
+#         return x
